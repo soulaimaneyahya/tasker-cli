1e9a9fdf377b598d3661226082e5490b9cc546c3
Soulaimane Yahya <soulaimaneyh07@gmail.com>
2024-11-10 18:01:36 +0100

    fixing

--- src/todoSlice.go
+++ src/todoSlice.go
@@ -40,11 +40,11 @@ func (todos *Todos) format() string {
 	}
 
 	for i, todo := range *todos {
-		status := "Incomplete"
+		status := "☐"
 		completedAt := "null"
 
 		if todo.completed {
-			status = "Completed"
+			status = "✅"
 			if todo.completedAt != nil {
 				completedAt = todo.completedAt.Format("2006-01-02 15:04:05")
 			}
